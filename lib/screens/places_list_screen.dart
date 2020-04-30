a02f0138a9d8dafc12f38eac96b2f0bcf8112719
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-30 23:28:15 +0100

    Adds new places to listtile

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './add_place_screen.dart';
+import '../providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
+  Widget _childBuilder(context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Got no places yet, start adding some!',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          const SizedBox(height: 12.0),
+          FlatButton(
+            onPressed: () =>
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName),
+            child: const Text(
+              'Start Adding',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Theme.of(context).primaryColor,
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +45,22 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(greatPlaces.items[i].image),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    //TODO go to details page
+                  },
+                ),
+                itemCount: greatPlaces.items.length,
+              ),
+        child: _childBuilder(context),
       ),
     );
   }
