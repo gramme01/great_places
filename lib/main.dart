fda7249a1234448e69795d2b0b2f0b4a186d8d2d
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-30 20:47:49 +0100

    Stores image on device file system

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => GreatPlaces(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Great Places',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
