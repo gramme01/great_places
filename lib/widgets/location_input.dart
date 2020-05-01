59559b3936eef34f2bd1ba4bd8ac85f271feb886
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-05-01 06:51:15 +0100

    Adds location package and loaction inout widget

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          height: 170,
+          width: double.infinity,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1.0, color: Colors.grey)),
+          child: _previewImageUrl == null
+              ? Text(
+                  'No location chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+            FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              label: Text('Select on Map'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
